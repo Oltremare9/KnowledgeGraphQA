e69d049ebe1aafcb855b94c1dc86b022136365c4
oltreamre9 <1134625169@qq.com>
2020-12-21 22:48:45 +0800

    test Jenkins

--- src/main/java/cn/edu/nju/kg_qa/controller/HelloController.java
+++ src/main/java/cn/edu/nju/kg_qa/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello World!";
+        return "Hello the World!";
     }
 }

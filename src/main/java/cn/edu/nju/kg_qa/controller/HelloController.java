e53dbc55c74a477a51ba2c45799bfd22cb1ad23d
oltreamre9 <1134625169@qq.com>
2020-12-22 14:27:38 +0800

    swagger集成

--- src/main/java/cn/edu/nju/kg_qa/controller/HelloController.java
+++ src/main/java/cn/edu/nju/kg_qa/controller/HelloController.java
@@ -1,14 +1,18 @@
 package cn.edu.nju.kg_qa.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wanghn
  */
+@Api(tags = "hello 测试系统联通性 以及jenkins使用")
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
+    @ApiOperation(value="无参数")
     public String sayHello() {
         return "Hello the World!";
     }
